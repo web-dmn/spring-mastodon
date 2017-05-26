c6f80aced94deaeda880041a755494d5384add78
web-dmn <web.dmn2016@gmail.com>
2017-05-27 01:03:09 +0900

    New Project

---
+++ src/main/java/web/app/MastodonApplication.java
@@ -0,0 +1,12 @@
+package web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastodonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastodonApplication.class, args);
+	}
+}
